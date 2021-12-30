28dca09feac2c0343eeaf4c523bfb4f6722eccbf
Prashant Pandey <prashant.pandey.dev@gmail.com>
2021-12-30 15:35:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const resFile = require('./res.json');
+
+const app = express();
+
+// Send CORS Headers, useful if API is requested from a browser
+app.use(cors());
+
+// Parse request body to json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log(res);
+});
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
